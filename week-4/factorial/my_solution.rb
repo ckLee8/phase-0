0bcc32a18f81247b9f6b2468df740b756c7f386f
Christopher Lee <zerosxl@gmail.com>
2015-10-03 18:03:14 -0400

    add factorial

---
+++ week-4/factorial/my_solution.rb
@@ -0,0 +1,19 @@
+# Factorial
+
+# I worked on this challenge [by myself, with: Noah Heinrich].
+
+
+# Your Solution Below
+def factorial(number)
+  # Your code goes here
+  fac = 1
+  if number == 0
+  	p 1
+  else
+  	while number > 0
+  		fac = fac * number
+  		number -= 1
+  		p fac
+    end
+  end
+end
