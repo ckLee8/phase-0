e5222c5aa51cf5ef202056f49a0882a3e35baa70
Christopher Lee <zerosxl@gmail.com>
2015-10-05 08:46:58 -0400

    add 4.6

---
+++ week-4/simple-substrings/my_solution.rb
@@ -0,0 +1,13 @@
+# Simple Substrings
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+def welcome string 
+	if string.include? "CA"
+		p "Welcome to California"
+	else
+		p "You should move to California"
+	end
+end
